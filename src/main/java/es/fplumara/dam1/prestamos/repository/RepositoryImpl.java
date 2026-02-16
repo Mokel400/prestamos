2bdb20ec5fd15a2654562218e79e7006026f14d8
Mikel <soymikelnevado@gmail.com>
2026-02-16 10:59:54 +0100

    Repository e implementacion empezados

---
+++ src/main/java/es/fplumara/dam1/prestamos/repository/RepositoryImpl.java
@@ -0,0 +1,32 @@
+package es.fplumara.dam1.prestamos.repository;
+
+import es.fplumara.dam1.prestamos.model.Identificable;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+public class RepositoryImpl <T extends Identificable> implements Repository<T>{
+
+
+    @Override
+    public void save(T elemento) {
+
+    }
+
+    @Override
+    public Optional findById(String id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List listAll() {
+        return List.of();
+    }
+
+    @Override
+    public void delete() {
+
+    }
+}
